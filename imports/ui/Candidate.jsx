6c2a0cca42eb6e3761ce56d35050e35fcfb4d98f
ziwer1 <ziwer1@gmail.com>
2018-01-05 04:20:44 +0300

    Add vote event handlers and no. of votes

--- imports/ui/Candidate.jsx
+++ imports/ui/Candidate.jsx
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
 import { Button, Card, Image } from 'semantic-ui-react';
 
+import {Candidates} from '../api/candidates.js';
+
 export default class Candidate extends Component {
 
+	constructor(){
+		super()
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(no, e){
+		e.preventDefault();
+
+		if (no == 1){
+			Candidates.update(this.props.candidate._id, {$inc :{votes:1}});
+		}else{
+			Candidates.update(this.props.candidate._id, {$inc :{votes:-1}});
+		}
+
+
+	}
 
 	render(){
 		return(
@@ -18,11 +36,16 @@ export default class Candidate extends Component {
 		        <Card.Description>
 		          {this.props.candidate.slogan}
 		        </Card.Description>
+		        <div>
+		        <strong>
+		          vote count: {this.props.candidate.votes}
+		         </strong>
+		        </div>
 		      </Card.Content>
 		      <Card.Content extra>
 		        <div className='ui two buttons'>
-		          <Button basic color='green'>Vote</Button>
-		          <Button basic color='red'>Unvote</Button>
+		          <Button basic color='green' onClick={(e) => this.handleClick(1, e)}>Vote</Button>
+		          <Button basic color='red' onClick={(e) => this.handleClick(0, e)}>Unvote</Button>
 		        </div>
 		      </Card.Content>
 		    </Card>
